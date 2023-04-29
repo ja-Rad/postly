3861310bba50914efddc63f3a51cd1176e997000
Rad <kalmanjudin2000@gmail.com>
2023-04-29 13:22:36 +0300

    Initial commit

---
+++ src/main/java/com/jarad/postly/PostlyApplication.java
@@ -0,0 +1,13 @@
+package com.jarad.postly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jarad/postly/PostlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarad.postly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

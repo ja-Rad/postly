3cef3a0ba1393d32a18a5957e42f61d0dd80fa3f
Rad <kalmanjudin2000@gmail.com>
2023-05-04 23:49:24 +0300

    Updated User Entity removed CascadeType.PERSIST from roles

--- src/main/java/com/jarad/postly/entity/User.java
+++ src/main/java/com/jarad/postly/entity/User.java
@@ -1,9 +1,7 @@
 package com.jarad.postly.entity;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -55,7 +53,7 @@ public class User implements Serializable {
 
     @ToString.Exclude
     @Fetch(FetchMode.JOIN)
-    @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @ManyToMany
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))

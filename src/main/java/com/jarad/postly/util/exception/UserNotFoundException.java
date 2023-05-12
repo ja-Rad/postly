a7e72eb7504826e4548eed388cd573d3a4f1ce97
Rad <kalmanjudin2000@gmail.com>
2023-05-12 22:33:13 +0300

    Added UserNotFoundException Exception

---
+++ src/main/java/com/jarad/postly/util/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jarad.postly.util.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}

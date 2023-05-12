eddf433e9311fbc25048dbe3273a705cef593c49
Rad <kalmanjudin2000@gmail.com>
2023-05-12 22:28:54 +0300

    Added ProfileDto

---
+++ src/main/java/com/jarad/postly/util/dto/ProfileDto.java
@@ -0,0 +1,20 @@
+package com.jarad.postly.util.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProfileDto {
+
+    @NotBlank
+    private String username;
+
+}

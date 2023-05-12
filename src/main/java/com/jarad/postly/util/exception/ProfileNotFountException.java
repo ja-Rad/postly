348046e9aa60689624fce765651ac43b14261d91
Rad <kalmanjudin2000@gmail.com>
2023-05-12 22:32:54 +0300

    Added ProfileNotFountException Exception

---
+++ src/main/java/com/jarad/postly/util/exception/ProfileNotFountException.java
@@ -0,0 +1,7 @@
+package com.jarad.postly.util.exception;
+
+public class ProfileNotFountException extends RuntimeException {
+    public ProfileNotFountException(String message) {
+        super(message);
+    }
+}

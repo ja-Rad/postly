b1853c8ef25a6a70fa7c4b9383601be8271afba5
Rad <kalmanjudin2000@gmail.com>
2023-05-12 22:26:52 +0300

    Added CommentRepository

---
+++ src/main/java/com/jarad/postly/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.jarad.postly.repository;
+
+import com.jarad.postly.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}

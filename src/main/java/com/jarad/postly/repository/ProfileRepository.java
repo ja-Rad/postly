9e6820a3255491b29c79ca1a50b3811807e1650c
Rad <kalmanjudin2000@gmail.com>
2023-05-12 22:27:49 +0300

    Added ProfileRepository

---
+++ src/main/java/com/jarad/postly/repository/ProfileRepository.java
@@ -0,0 +1,13 @@
+package com.jarad.postly.repository;
+
+import com.jarad.postly.entity.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Optional<Profile> findByUser_Email(String email);
+
+}

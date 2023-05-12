9f660a7495b6ef6e1c7affaef4e8ba85beb66485
Rad <kalmanjudin2000@gmail.com>
2023-05-12 22:33:53 +0300

    Updated UserAlreadyExistException Exception

--- src/main/java/com/jarad/postly/util/exception/UserAlreadyExistException.java
+++ src/main/java/com/jarad/postly/util/exception/UserAlreadyExistException.java
@@ -1,6 +1,6 @@
 package com.jarad.postly.util.exception;
 
-public class UserAlreadyExistException extends Exception {
+public class UserAlreadyExistException extends RuntimeException {
 
     public UserAlreadyExistException(String message) {
         super(message);

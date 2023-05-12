abaaeba1e79ecf2f246633149db47e04e9ad310a
Rad <kalmanjudin2000@gmail.com>
2023-05-12 22:25:03 +0300

    Added Tag Entity

---
+++ src/main/java/com/jarad/postly/entity/Tag.java
@@ -0,0 +1,37 @@
+package com.jarad.postly.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tags")
+public class Tag implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Column(name = "name")
+    private String name;
+
+}

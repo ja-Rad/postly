7dab9eb8fd460ecfd1b24907fd7359ab389cd582
Rad <kalmanjudin2000@gmail.com>
2023-05-12 22:27:28 +0300

    Added FollowerRepository

---
+++ src/main/java/com/jarad/postly/repository/FollowerRepository.java
@@ -0,0 +1,9 @@
+package com.jarad.postly.repository;
+
+import com.jarad.postly.entity.Follower;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FollowerRepository extends JpaRepository<Follower, Long> {
+}

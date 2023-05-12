f4a0dfee95f51fd3e1be86eb4608e16df682c5b2
Rad <kalmanjudin2000@gmail.com>
2023-05-12 22:24:45 +0300

    Added Post Entity

---
+++ src/main/java/com/jarad/postly/entity/Post.java
@@ -0,0 +1,71 @@
+package com.jarad.postly.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "posts")
+public class Post implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @NotEmpty
+    @Column(name = "title")
+    private String title;
+
+    @NotNull
+    @NotEmpty
+    @Column(name = "description", length = 2000)
+    private String description;
+
+    @NotNull
+    @Column(name = "creation_date", nullable = false)
+    private Instant creationDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id")
+    private Profile profile;
+
+    @Fetch(FetchMode.JOIN)
+    @ManyToMany
+    @JoinTable(name = "posts_tags",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private Set<Tag> tags = new LinkedHashSet<>();
+
+    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<Comment> comments;
+}

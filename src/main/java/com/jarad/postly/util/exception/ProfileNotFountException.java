5f0ee22ac5ac7a9f4f8757fb1cce7dd3015f8ce3
Rad <kalmanjudin2000@gmail.com>
2023-05-15 16:11:24 +0300

    Fixed typo in ProfileNotFoundException

--- src/main/java/com/jarad/postly/util/exception/ProfileNotFountException.java
+++
@@ -1,7 +0,0 @@
-package com.jarad.postly.util.exception;
-
-public class ProfileNotFountException extends RuntimeException {
-    public ProfileNotFountException(String message) {
-        super(message);
-    }
-}

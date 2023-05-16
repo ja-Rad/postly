ab2b6ba951f8c8b8884cb5abd66b10575f99962e
Rad <kalmanjudin2000@gmail.com>
2023-05-16 15:28:01 +0300

    Refactored Entity structure from Profile-<ProfileFollower>-Follower to Profile + Follower with EmbeddedId

--- src/main/java/com/jarad/postly/repository/FollowerRepository.java
+++ src/main/java/com/jarad/postly/repository/FollowerRepository.java
@@ -1,9 +1,15 @@
 package com.jarad.postly.repository;
 
 import com.jarad.postly.entity.Follower;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FollowerRepository extends JpaRepository<Follower, Long> {
+    Page<Follower> findAuthorPageById_FollowerId(Long followerId, Pageable pageable);
+
+    Page<Follower> findFollowerPageById_AuthorId(Long authorId, Pageable pageable);
+
 }

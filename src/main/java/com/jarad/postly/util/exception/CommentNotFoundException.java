e16de0ea9ce782d8491ee8abc8e78598ae2d8e5f
Rad <kalmanjudin2000@gmail.com>
2023-05-16 23:41:03 +0300

    Added CommentNotFoundException Exception

---
+++ src/main/java/com/jarad/postly/util/exception/CommentNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jarad.postly.util.exception;
+
+public class CommentNotFoundException extends RuntimeException {
+    public CommentNotFoundException(String message) {
+        super(message);
+    }
+}

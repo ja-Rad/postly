84214ff18075a4e88dd353829c41a237cfa607e7
Rad <kalmanjudin2000@gmail.com>
2023-05-18 17:38:32 +0300

    Updated CommentRepository

--- src/main/java/com/jarad/postly/repository/CommentRepository.java
+++ src/main/java/com/jarad/postly/repository/CommentRepository.java
@@ -1,9 +1,18 @@
 package com.jarad.postly.repository;
 
 import com.jarad.postly.entity.Comment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Page<Comment> findCommentPageByProfile_Id(Long id, Pageable pageable);
+
+    Page<Comment> findByPost_Id(Long id, Pageable pageable);
+
+    Optional<Comment> findByProfile_IdAndId(Long id, Long id1);
 }

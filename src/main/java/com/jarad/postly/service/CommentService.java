21c75672e170c1df261adc85bc380bc2610eaab6
Rad <kalmanjudin2000@gmail.com>
2023-05-18 17:39:11 +0300

    Added CommentService and Implementation

---
+++ src/main/java/com/jarad/postly/service/CommentService.java
@@ -0,0 +1,13 @@
+package com.jarad.postly.service;
+
+import com.jarad.postly.entity.Comment;
+import com.jarad.postly.util.dto.CommentDto;
+
+public interface CommentService {
+
+    Comment returnCommentById(Long id);
+
+    Long updateExistingComment(Long profileId, Long commentId, CommentDto commentDto);
+
+    void deleteExistingComment(Long profileId, Long commentId);
+}
---
+++ src/main/java/com/jarad/postly/service/CommentServiceImpl.java
@@ -0,0 +1,58 @@
+package com.jarad.postly.service;
+
+import com.jarad.postly.entity.Comment;
+import com.jarad.postly.repository.CommentRepository;
+import com.jarad.postly.util.dto.CommentDto;
+import com.jarad.postly.util.exception.CommentNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+
+    @Autowired
+    public CommentServiceImpl(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    @Override
+    public Comment returnCommentById(Long id) {
+        Optional<Comment> optionalComment = commentRepository.findById(id);
+        if (optionalComment.isEmpty()) {
+            throw new CommentNotFoundException("Comment with this id: " + id + " doesn`t exist");
+        }
+
+        return optionalComment.get();
+    }
+
+    @Override
+    public Long updateExistingComment(Long profileId, Long commentId, CommentDto commentDto) {
+        Optional<Comment> optionalComment = commentRepository.findByProfile_IdAndId(profileId, commentId);
+        if (optionalComment.isEmpty()) {
+            throw new CommentNotFoundException("Comment with id: " + commentId + " for profile with id: " + profileId + " doesn`t exist");
+        }
+
+        Comment comment = optionalComment.get();
+        comment.setDescription(commentDto.getDescription());
+        Comment savedComment = commentRepository.save(comment);
+
+        return savedComment.getId();
+    }
+
+    @Override
+    public void deleteExistingComment(Long profileId, Long commentId) {
+        Optional<Comment> optionalComment = commentRepository.findByProfile_IdAndId(profileId, commentId);
+        if (optionalComment.isEmpty()) {
+            throw new CommentNotFoundException("Comment with id: " + commentId + " for profile with id: " + profileId + " doesn`t exist");
+        }
+
+        Comment comment = optionalComment.get();
+
+        commentRepository.delete(comment);
+    }
+}
+

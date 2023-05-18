719dabfbd2cc2c7940182cddec823441a220ac65
Rad <kalmanjudin2000@gmail.com>
2023-05-18 17:38:41 +0300

    Updated FollowerRepository

--- src/main/java/com/jarad/postly/repository/FollowerRepository.java
+++ src/main/java/com/jarad/postly/repository/FollowerRepository.java
@@ -1,13 +1,15 @@
 package com.jarad.postly.repository;
 
 import com.jarad.postly.entity.Follower;
+import com.jarad.postly.entity.embeddable.FollowerId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FollowerRepository extends JpaRepository<Follower, Long> {
+public interface FollowerRepository extends JpaRepository<Follower, FollowerId> {
+    
     Page<Follower> findAuthorPageById_FollowerId(Long followerId, Pageable pageable);
 
     Page<Follower> findFollowerPageById_AuthorId(Long authorId, Pageable pageable);

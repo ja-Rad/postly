1c0f302749a1336363c414588d9e475422cf6431
Rad <kalmanjudin2000@gmail.com>
2023-07-05 20:01:12 +0300

    Created Integration Tests for SecurityController

---
+++ src/test/java/com/jarad/postly/integration/controller/SecurityControllerTest.java
@@ -0,0 +1,65 @@
+package com.jarad.postly.integration.controller;
+
+import com.jarad.postly.controller.SecurityController;
+import com.jarad.postly.entity.Role;
+import com.jarad.postly.entity.User;
+import com.jarad.postly.security.UserDetailsImpl;
+import com.jarad.postly.service.PostService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(controllers = SecurityController.class)
+public class SecurityControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private PostService postService;
+
+    private UserDetailsImpl userDetails;
+
+    @BeforeEach
+    void setUp() {
+        userDetails = new UserDetailsImpl(getUser());
+        // Set the user in the security context
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities()));
+    }
+
+    @Test
+    public void testAccessDeniedPage() throws Exception {
+        mockMvc.perform(get("/403"))
+                .andExpect(status().isForbidden())
+                .andExpect(model().attribute("status", HttpStatus.FORBIDDEN.value()))
+                .andExpect(model().attribute("message", "Access Denied"))
+                .andExpect(view().name("error"));
+    }
+
+    /**
+     * Helper method that creates Dummy Test Doubles for the User
+     *
+     * @return User
+     */
+    private User getUser() {
+        return User.builder()
+                .id(1L)
+                .email("1stUserEmail@Example.com")
+                .password("1st User Password Example")
+                .roles(new HashSet<>(Set.of(Role.builder().name("ROLE_PROFILE_ACTIVE").build())))
+                .enabled(true)
+                .build();
+    }
+}

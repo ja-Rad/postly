484254b1beaec774abb3170055df67262ce10ebe
Rad <kalmanjudin2000@gmail.com>
2023-07-25 02:34:12 +0300

    Fixed SecurityConfigIT

--- src/test/java/com/jarad/postly/integration/config/SecurityConfigIT.java
+++ src/test/java/com/jarad/postly/integration/config/SecurityConfigIT.java
@@ -26,7 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class SecurityConfigTest {
+class SecurityConfigIT {
     @Autowired
     private MockMvc mockMvc;
     @MockBean
